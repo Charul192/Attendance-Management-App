9a6c5bec81dd5f00c7e2e347504a5f13ca809179
Charul192 <charul1902@gmail.com>
2025-08-15 12:13:10 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,8 +8,8 @@ import { Auth0Provider } from '@auth0/auth0-react';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-    domain="dev-w2bemlqjqjhnlmli.us.auth0.com"
-    clientId="ZIadS2ZamUGAy8RU0iuTwLXn0Txfxhjs"
+    domain=VITE_AUTH0_DOMAIN
+    clientId=VITE_AUTH0_CLIENT_ID
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
